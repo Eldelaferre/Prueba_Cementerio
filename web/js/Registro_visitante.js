c35dd74a28b6d7d6ab7ef214314ebe9682643ef0
jonathan617 <jonathanbarrera91@gmail.com>
2021-12-04 17:21:37 -0500

    registro usuario y visitante completo

--- web/js/Registro_visitante.js
+++ web/js/Registro_visitante.js
@@ -1,3 +1,34 @@
 $(document).ready(function () {
-    alert("Registro visitante prueba")
+    alert("Registro visitante prueba");
+    
+    $('#btn_Registro_Visitante').on('click', function () {
+        
+        Insertar();
+        alert("entra al boton");
+        
+    });
+    
+    function Insertar() {
+        alert("en insertar");
+        let doc=$('#doc').val();
+        let nom=$('#nom').val();
+        let dir=$('#dir').val();
+        let tel=$('#tel').val();
+        let cor=$('#cor').val();
+
+        
+        alert(" Documento "+doc);
+        
+        $.ajax({
+            
+            type:"POST",
+            url:"Servlet_Visitante",
+            data:{dato:"insertar",d:doc,n:nom,di:dir,t:tel,co:cor},
+                success:function(res){
+                    if(res){
+                        alert("datos guardados");
+                    }
+                }
+        });
+    }
 })
